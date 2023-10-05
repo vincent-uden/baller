4ad31839756849ab52604dd24a0d2b4f992fe727
filipsterner <filipsterner.cth@gmail.com>
2023-10-05 19:21:37 +0200

    added calibration, fixed pixel to spatial orientation

--- baller/image_analysis/pixel_coordinates_to_spatial.py
+++ baller/image_analysis/pixel_coordinates_to_spatial.py
@@ -1,10 +1,12 @@
-def pixel_to_spatial(pixel_x ,pixel_y):
+def pixel_to_spatial(pixel_x ,pixel_y, pixel_to_meter_ratio, camera_offset):
     """
-    Converts pixel coordinates from a 640x480 webcam to spatial coordinates of Hubert
+    Converts pixel coordinates to spatial coordinates of Hubert
 
     Parameters:
     - pixel_x: pixel x-coordinate to be converted to spatial y-coordinate
     - pixel_y: pixel y-coordinate to be converted to spatial z-coordinate
+    - pixel_to_meter_ratio: pixel to spatial meter ratio from calibration
+    - camera_offset: distance in meter from bottom of frame to reference z=0
 
     Returns:
     - spatial_x: spatial x-coordinate estimated as distance from Hubert to wall
@@ -12,15 +14,11 @@ def pixel_to_spatial(pixel_x ,pixel_y):
     - spatial_z: spatial z-coordinate corresponding to pixel y-coordinate
     """
 
-    pixel_to_meter_ratio = 0.0011228070175
+    pixel_x0 = 1280/2
+    pixel_y0 = 720
 
-    #origin of spatial_y is estimated as pixel_x = 320px
-    pixel_x0 = 640
-    #origin of spatial_z is estimated as pixel_y = 808.64 (pixel y coordinates is inverted)
-    pixel_y0 = 360
-
-    spatial_y = (pixel_x-pixel_x0) * pixel_to_meter_ratio
-    spatial_z = (pixel_y0-pixel_y) * pixel_to_meter_ratio
+    spatial_y = (pixel_x0 - pixel_x) * pixel_to_meter_ratio
+    spatial_z = (pixel_y0 - pixel_y) * pixel_to_meter_ratio + camera_offset
 
     spatial_x = 1.69 #distance from Hubert to wall
 
